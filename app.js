66f208a32d2c38e16e550a3f797e087cb4986bfb
DavidCT <DavidCuervoTunon@gmail.com>
2021-06-13 20:10:10 +0200

    Prueba

---
+++ app.js
@@ -0,0 +1,16 @@
+function sumar(a, b) {
+    return a + b;
+};
+
+function obtenerCarpeta() {
+    var resultado;
+    var mensaje;
+
+    resultado = 7;
+    if (resultado < 5) {
+        mensaje = "El resultado del examen es suspenso";
+    } else {
+        mensaje = "El resultado del examen es aprobado";
+    }
+    console.log(mensaje);
+}
